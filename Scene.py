1163b96d218527c3c5cb69ad83229438f0a77c06
GalileoCap <galileocapp@gmail.com>
2023-10-11 19:26:24 -0300

    OK: Input name

--- Scene.py
+++ Scene.py
@@ -32,25 +32,78 @@ class MenuScene(Scene):
   def __init__(self):
     super().__init__()
     self.scores = [
-      Label(Vector2(1280/2 - 4*35/2, 720/2 - 1*35), 150, 35, 'FGC - 1000', 'black', 'white'),
-      Label(Vector2(1280/2 - 4*35/2, 720/2 - 0*35), 150, 35, 'FGC - 1000', 'black', 'white'),
-      Label(Vector2(1280/2 - 4*35/2, 720/2 - -1*35), 150, 35, 'FGC - 1000', 'black', 'white')
+      Label(Vector2(1280/2 - 3*35/2, 720/2 - 1*35), 150, 35, 'FGC - 1000', 'black', 'white'),
+      Label(Vector2(1280/2 - 3*35/2, 720/2 - 0*35), 150, 35, 'FGC - 1000', 'black', 'white'),
+      Label(Vector2(1280/2 - 3*35/2, 720/2 - -1*35), 150, 35, 'FGC - 1000', 'black', 'white')
     ]
     self.objs += [
       Label(Vector2(1280/2 - 4*35/2, 720/2 - 6*35), 150, 35, 'ASTEROIDS', 'gray', 'white'),
       Button(Vector2(1280/2 - 2*35/2, 720/2 - 5*35), 75, 35, 'Jugar', 'green', 'white', lambda ctx: ctx.setChangeScene(GameScene())),
 
-      Label(Vector2(1280/2 - 4*35/2, 720/2 - 2*35), 150, 35, 'Puntajes', 'black', 'white'),
+      Label(Vector2(1280/2 - 3*35/2, 720/2 - 2*35), 150, 35, 'Puntajes', 'black', 'white'),
       *self.scores,
 
-      Button(Vector2(1280/2 - 35/2, 720/2 + 3*35), 75, 35, 'Salir', 'black', 'gray', lambda ctx: ctx.quit()),
+      Button(Vector2(1280/2 - 3*35/2, 720/2 + 3*35), 75, 35, 'Configurar', 'black', 'gray', lambda ctx: ctx.setChangeScene(ConfigScene())),
+      Button(Vector2(1280/2 - 35/2, 720/2 + 5*35), 75, 35, 'Salir', 'black', 'gray', lambda ctx: ctx.quit()),
     ]
 
   def tick(self, ctx):
-    self.scores[0].text = utils.fmtScore(ctx.maxScores[0])
-    self.scores[1].text = utils.fmtScore(ctx.maxScores[1])
-    self.scores[2].text = utils.fmtScore(ctx.maxScores[2])
+    for i in range(len(self.scores)):
+      self.scores[i].text = utils.fmtScore(ctx.maxScores[i])
     super().tick(ctx)
+
+class ConfigScene(Scene):
+  def __init__(self):
+    super().__init__()
+    self.objs += [
+      Label(Vector2(1280/2 - 4*35/2, 720/2 - 6*35), 150, 35, 'ASTEROIDS', 'gray', 'white'),
+      Button(Vector2(1280/2 - 3*35/2, 720/2 - 5*35), 75, 35, 'Configurar', 'black', 'white', lambda ctx: ctx.setChangeScene(GameScene())),
+
+      Button(Vector2(1280/2 - 5*35/2, 720/2 - 2*35), 150, 35, 'Cambiar Nombre', 'black', 'white', lambda ctx: ctx.setChangeScene(NameScene())),
+      # Label(Vector2(1280/2 - 4*35/2, 720/2 - 2*35), 150, 35, 'TODO: Teclas', 'black', 'white'),
+
+      Button(Vector2(1280/2 - 35/2, 720/2 + 3*35), 75, 35, 'Atrás', 'black', 'gray', lambda ctx: ctx.setChangeScene(MenuScene())),
+    ]
+
+class NameScene(Scene):
+  def __init__(self):
+    super().__init__()
+    self.playerName = ''
+    self.letters = [
+      Label(Vector2(1280/2 - 2*35/2, 720/2), 15, 25, ' ', 'gray', 'white'),
+      Label(Vector2(1280/2 - 0*35/2, 720/2), 15, 25, ' ', 'gray', 'white'),
+      Label(Vector2(1280/2 - -2*35/2, 720/2), 15, 25, ' ', 'gray', 'white'),
+    ]
+    self.objs += [
+      Label(Vector2(1280/2 - 4*35/2, 720/2 - 6*35), 150, 35, 'ASTEROIDS', 'gray', 'white'),
+      Button(Vector2(1280/2 - 3*35/2, 720/2 - 5*35), 75, 35, 'Configurar', 'black', 'white', lambda ctx: ctx.setChangeScene(GameScene())),
+
+      Label(Vector2(1280/2 - 5*35/2, 720/2 - 3*35), 150, 35, 'Tipeá tu nombre (tres letras)', 'black', 'white'),
+      Label(Vector2(1280/2 - 5*35/2, 720/2 - 2*35), 150, 35, 'y presioná enter para elegirlo', 'black', 'white'),
+
+      *self.letters,
+
+      Button(Vector2(1280/2 - 35/2, 720/2 + 3*35), 75, 35, 'Atrás', 'black', 'gray', lambda ctx: ctx.setChangeScene(ConfigScene())),
+    ]
+
+  def tick(self, ctx):
+    super().tick(ctx)
+    self._inputName(ctx)
+
+  def draw(self, ctx):
+    super().draw(ctx)
+    for i in range(len(self.letters)):
+      self.letters[i].text = self.playerName[i] if i < len(self.playerName) else ' '
+
+  def _inputName(self, ctx):
+    for event in ctx.events:
+      if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_BACKSPACE:
+          self.playerName = self.playerName[:-1]
+        elif event.key == pygame.K_RETURN and len(self.playerName) == 3:
+          ctx.setPlayerName(self.playerName)
+        elif len(self.playerName) < 3:
+          self.playerName += event.unicode
 
 class GameScene(Scene):
   def __init__(self):
